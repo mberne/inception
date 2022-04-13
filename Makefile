9a944c2de86ecd5f5d8f678ce680fb264a29b41f
Manon Berne <mberne@student.42lyon.fr>
2022-04-13 11:18:31 -0400

    all working, need to fix Makefile and fake correct

--- Makefile
+++ Makefile
@@ -2,11 +2,20 @@ all:
 		mkdir -p /home/mberne/data/db /home/mberne/data/wp
 		docker-compose --project-directory=srcs up -d --build
 
-clean:
+down:
 		docker-compose --project-directory=srcs down --volumes
-		# docker system prune -a
-		# rm -rf /home/mberne/data/db /home/mberne/data/wp
 
-re: clean all
+clean:	down
+		docker stop $(docker ps -qa)
+		docker rm $(docker ps -qa)
+		docker rmi -f $(docker images -qa)
+		docker volume rm $(docker volume ls -q)
+		docker network rm $(docker network ls -q)
+		docker system prune -a
 
-.PHONY: all re clean
+fclean:	clean
+		rm -rf /home/mberne/data
+
+re: fclean all
+
+.PHONY: all down clean fclean re
--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -22,16 +22,16 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'wordpress_database' );
+define( 'DB_NAME', '$DB_NAME' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'user' );
+define( 'DB_USER', '$DB_USER' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', 'user_password' );
+define( 'DB_PASSWORD', '$DB_PASSWORD' );
 
 /** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mariadb:3306' );
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define( 'DB_CHARSET', 'utf8' );
